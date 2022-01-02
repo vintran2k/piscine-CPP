0ff89ad0d156e54688021df79f771b7304ca305d
Debian <admin@ip-172-31-39-92.eu-west-3.compute.internal>
2022-01-02 16:58:40 +0000

    done

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -5,30 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vintran <vintran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/01 22:11:20 by vintran           #+#    #+#             */
-/*   Updated: 2022/01/02 16:57:58 by vintran          ###   ########.fr       */
+/*   Created: 2022/01/02 16:57:26 by vintran           #+#    #+#             */
+/*   Updated: 2022/01/02 16:57:33 by vintran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Phonebook.hpp"
-
-int	main(void)
-{
-	std::string		input;
-	Phonebook		book;
-
-	while (!std::cin.eof())
-	{
-		book.prompt();
-		getline(std::cin, input);
-		if (input == "EXIT")
-			break ;
-		else if (input == "ADD")
-			book.add();
-		else if (input == "SEARCH")
-			book.search();
-		else if (!input.empty())
-			std::cout << "Error: invalid command" << std::endl;
-	}
-	return (0);
-}
+int main()
